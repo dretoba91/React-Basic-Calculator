7d53556d8fcb36d5a0af16e41fc5ea6e08188e7e
dretoba91e <64743870+dretoba91@users.noreply.github.com>
2021-09-08 18:21:41 +0100

    on it

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import ButtonPanel from "./ButtonPanel";
 import Display from "./Display";
+import Toolbar5 from "../components/toolbar-5/Toolbar5";
 
 function App() {
   return (
     <div>
       <Display />
       <ButtonPanel />
+      <Toolbar5 />
     </div>
   );
 }
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,48 +2,51 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const ButtonPanel = () => {
+const ButtonPanel = (clickApp) => {
+  const clickHandle = (buttonName) => {
+    clickApp(buttonName);
+  };
   return (
     <>
-      <div>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
+      <div className="">
+        <Button buttonName="AC" clickPanel={clickHandle} color="" />
+        <Button buttonName="+/-" clickPanel={clickHandle} color="" />
+        <Button buttonName="%" clickPanel={clickHandle} color="" />
+        <Button buttonName="/" clickPanel={clickHandle} color="" />
       </div>
 
-      <div>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
+      <div className="">
+        <Button buttonName="7" clickPanel={clickHandle} color="" />
+        <Button buttonName="8" clickPanel={clickHandle} color="" />
+        <Button buttonName="9" clickPanel={clickHandle} color="" />
+        <Button buttonName="X" clickPanel={clickHandle} color="" />
       </div>
 
-      <div>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
+      <div className="">
+        <Button buttonName="4" clickPanel={clickHandle} color="" />
+        <Button buttonName="5" clickPanel={clickHandle} color="" />
+        <Button buttonName="6" clickPanel={clickHandle} color="" />
+        <Button buttonName="-" clickPanel={clickHandle} color="" />
       </div>
 
-      <div>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
+      <div className="">
+        <Button buttonName="1" clickPanel={clickHandle} color="" />
+        <Button buttonName="2" clickPanel={clickHandle} color="" />
+        <Button buttonName="3" clickPanel={clickHandle} color="" />
+        <Button buttonName="+" clickPanel={clickHandle} color="" />
       </div>
 
-      <div>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
-        <Button buttonName="" clickPanel={} color=""/>
+      <div className="">
+        <Button buttonName="0" clickPanel={clickHandle} color="" />
+        <Button buttonName="." clickPanel={clickHandle} color="" />
+        <Button buttonName="=" clickPanel={clickHandle} color="" />
       </div>
     </>
   );
 };
 
 ButtonPanel.propTypes = {
-    clickApp: PropTypes.func.isRequired,
-}
+  clickApp: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
